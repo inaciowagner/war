01729b627fdb8dfcbf2dd49f4775694381ca9790
Inácio W. L. de Carvalho <inacio@datacomrn.com.br>
2024-09-01 19:42:59 -0300

    Atualizar o war.py manual

--- war.py
+++ war.py
@@ -114,3 +114,71 @@ st.markdown('''
 
 * se conquistar no mínimo um território (o número de cartas em sua mão NÃO pode exceder 5 cartas).
 ''')
+# Manual de Regras 
+st.markdown('''
+1. Cada jogador pega um dado e o lança. Aquele que obtiver o ponto mais alto será o dis- tribuidor, cabendo-lhe a tarefa de separar as 12 cartas dos Centros Estratégicos, embaralhá-las e distribuí-las, uma a uma, a todos os participantes, começando por si próprio e seguindo pelo jo-
+
+gador à sua esquerda. Após a distribuição, os jogadores colocam 2 exércitos em cada um dos Centros Estratégicos
+
+recebidos.
+
+NOTA: Algumas cartas possuem mais de 1 território, mas as peças devem ser colocadas apenas no que possui o sinal de Centro Estratégico.
+
+Distribuição para 5 jogadores:
+
+Nesse caso, distribuem-se inicialmente apenas 10 cartas de CE (2 para cada um). Os jogadores colocam então no tabuleiro os exércitos a que tiverem direito, da forma acima descrita.
+
+Em seguida cada jogador, na sua vez, pode optar: a) Por uma das duas cartas de CE restantes, colocando 2 exércitos no território correspondente. b) Por dois territórios quaisquer, desde que já não estejam ocupados, não sejam Centros Estraté- gicos e não sejam ambos no mesmo continente, colocando apenas um exército em cada um deles.
+
+O jogador, na sua vez, poderá não ter opções. Isso acontece quando não existirem mais cartas de CE, restando somente a opção "b", ou quan- do três outros jogadores já tiverem optado pela escolha de 2 territórios, restando somente a op- ção "a".
+
+Terminada esta fase, as cartas devem ser novamente embaralhadas com as 49 restantes do ma- ço (cartas de números e territórios) e colocadas com a face para baixo em uma área livre do ta- buleiro. Feito isto, o distribuidor coloca um exército em um território vago à sua escolha, seguido pelo jogador que está à sua esquerda e assim sucessivamente. Esse processo deve ser repetido até que todos os territórios estejam ocupados.
+
+OBJETIVOS
+
+Em seguida a distribuição dos exércitos e dos territórios, é feito o sorteio dos objetivos, receben- do cada jogador 1 objetivo dentre os 20 existentes, tomando conhecimento do seu teor e evitan- do revela-lo aos seus adversários. É recomendado aos jogadores que estão se iniciando no jogo que, antes do sorteio, seja feita
+
+uma leitura de todos os objetivos possíveis.
+
+OBS. 1: No caso do número de jogadores ser inferior a 6, os objetivos relacionados com os exér-
+
+citos não participantes devem ser excluídos do sorteio.
+
+OBS. 2: Se algum jogador, no transcorrer do jogo, ficar reduzido a 1 único território, seu objetivo passa automaticamente a ser conquistar três Centros Estratégicos quaisquer.
+
+4-0 JOGO
+
+Inicia o jogo o participante seguinte ao que ocupou o último território na distribuição dos territórios.
+
+Primeira Rodada:
+
+a) Todos os jogadores, cada um na sua vez, colocam sobre o tabuleiro as peças - exércitos e bom- bardeiros a que têm direito (pelo número de territórios, número de CE, maiorias e totalida- des dos continentes). b) A seguir, cada jogador, na sua vez, se desejar ataca seus adversários, desloca seus exércitos
+
+A primeira rodada consiste em 2 etapas:
+
+se houver conveniência e recebe cartas se fizer jus a isso. (Estas etapas estão detalhadas adiante). Segunda rodada em diante:
+
+partir da segunda rodada cada jogador na sua vez, sem esperar a colocação das peças dos ad- A versários, joga obedecendo as seguintes etapas nesta ordem:
+
+a) Recebe novos exércitos e os coloca no tabuleiro de acordo com a sua estratégia (em territórios que já lhe pertencem);
+
+b) Recebe bombardeiros e os coloca na sua base aérea;
+
+d) Desloca seus exércitos se houver conveniência; e
+
+e) Recebe cartas se fizer jus a isto. (Para receber cartas é necessário a conquista de pelo menos
+
+c) Se desejar, ataca os adversários;
+
+OBS. 1. A diferença entre a primeira rodada e as subseqüentes se justifica a fim de equilibrar as forças no início do jogo.
+
+2
+
+um território).
+
+OBS
+
+. 2: Cada fase do jogo está explicada detalhadamente nos itens a segui. Quando houver dúvida sobre alguma destas fases, leia novamente a seção correspon-
+
+3
+''')
